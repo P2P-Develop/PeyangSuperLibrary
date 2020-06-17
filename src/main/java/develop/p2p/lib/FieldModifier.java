c8645bc57ed8d69023f4af0f01fee613d283c102
peyang-Celeron <mikasa.chibimasu@gmail.com>
2020-06-17 23:42:22 +0900

    rafsk

---
+++ src/main/java/develop/p2p/lib/FieldModifier.java
@@ -0,0 +1,28 @@
+package develop.p2p.lib;
+
+import org.slf4j.*;
+
+import java.lang.reflect.*;
+
+public class FieldModifier
+{
+    public static  <T, K> boolean modify(T target, String field, K dummy)
+    {
+        try
+        {
+            Field f = target.getClass().getDeclaredField(field);
+            f.setAccessible(true);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(f,
+                    f.getModifiers() & ~Modifier.PRIVATE & ~Modifier.FINAL & ~Modifier.STATIC);
+            f.set(field, dummy);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
--- src/main/java/develop/p2p/lib/LoggerChanger.java
+++ src/main/java/develop/p2p/lib/LoggerChanger.java
@@ -2,26 +2,11 @@ package develop.p2p.lib;
 
 import org.slf4j.*;
 
-import java.lang.reflect.*;
-
 public class LoggerChanger
 {
     public static <T, K extends Logger> boolean changeToDummy(T target, String field, K dummyLogger)
     {
-        try
-        {
-            Field f = target.getClass().getDeclaredField(field);
-
-            f.setAccessible(true);
-
-            f.set(target, dummyLogger);
-
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
+        return FieldModifier.modify(target, field, dummyLogger);
     }
 
     public static <T> boolean shutUpLog(T target, String field)
