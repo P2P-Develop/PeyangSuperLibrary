435d1e89711ea62628b12ad8a62c4e2decf5b330
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-02-12 00:31:59 +0900

    かみ

--- src/main/java/develop/p2p/lib/FieldModifier.java
+++ src/main/java/develop/p2p/lib/FieldModifier.java
@@ -23,4 +23,21 @@ public class FieldModifier
             return false;
         }
     }
+
+
+    public static <T> void modifyAsGod(Class<?> clazz, String field, T as)
+    {
+        try
+        {
+            Field f = clazz.getDeclaredField(field);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(f,
+                    f.getModifiers() & ~Modifier.PRIVATE & ~Modifier.FINAL & ~Modifier.STATIC);
+            f.set(null, as);
+        }
+        catch (Exception ignored)
+        {
+        }
+    }
 }
